db333d6b02eac72bc9852b97c25388cb2f343a7f
Abbas Farid <abbasfreestyle@gmail.com>
2021-08-27 10:01:29 +0100

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: [
+    "react",
+    "react-native",
+    "prettier",
+    // "simple-import-sort",
+  ],
+};
