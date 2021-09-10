557b1932599bf95a320a3ad4aa66482f88d7213d
Abbas Farid <abbasfreestyle@gmail.com>
2021-09-10 09:40:45 +0100

    clean up jest config

--- jest.config.js
+++ jest.config.js
@@ -3,18 +3,6 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   verbose: true,
   setupFiles: ['<rootDir>/.jest/setup.ts'],
-  // setupFilesAfterEnv: ['./setup/jest.js'],
-  // moduleNameMapper: {
-  //   '@api': '<rootDir>/src/api',
-  //   '@assets': '<rootDir>/src/assets',
-  //   '@components': '<rootDir>/src/components',
-  //   '@config': '<rootDir>/src/config',
-  //   '@hooks': '<rootDir>/src/hooks',
-  //   '@modules': '<rootDir>/src/modules',
-  //   '@screens': '<rootDir>/src/screens',
-  //   '@types': '<rootDir>/src/types',
-  //   '@utils': '<rootDir>/src/utils',
-  // },
   // modulePathIgnorePatterns: ['<rootDir>/e2e'],
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?' +
