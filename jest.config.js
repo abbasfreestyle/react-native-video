9f155d182af526f2c2eae5aabcac443ddcafa6bd
Abbas Farid <abbasfreestyle@gmail.com>
2021-09-10 17:46:51 +0100

    try out coverage

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,12 @@ module.exports = {
       '|@react-native-community' +
       '|@react-navigation)',
   ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
