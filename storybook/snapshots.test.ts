39f612e7d00aa412e70e4bdb0fe0bd76b892771c
Abbas Farid <abbasfreestyle@gmail.com>
2021-09-10 10:43:24 +0100

    add mock

--- storybook/snapshots.test.ts
+++ storybook/snapshots.test.ts
@@ -1,3 +1,8 @@
 import initStoryshots from '@storybook/addon-storyshots';
 
+// https://github.com/storybookjs/storybook/issues/8083
+jest.mock('global', () =>
+  Object.assign(global, { window: { STORYBOOK_HOOKS_CONTEXT: '' } }),
+);
+
 initStoryshots();
