bf069319b1949a67176310642758a5c156abcd2b
Abbas Farid <abbasfreestyle@gmail.com>
2021-09-10 09:39:38 +0100

    add comment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { AppRegistry, LogBox } from 'react-native';
 import { name as appName } from './app.json';
 import { AppWithNavigator } from './src/navigation/AppNavigator';
 
+// Caused by storybook
 LogBox.ignoreLogs([/core-js/g, /removeListener/g]);
 
 AppRegistry.registerComponent(appName, () => AppWithNavigator);
