21bea6e60313da8e8e108a50b8cc9f4bba3eaa3b
Abbas Farid <abbasfreestyle@gmail.com>
2021-09-18 00:21:24 +0100

    chore: declare module types for reanimated jest utils

---
+++ src/@types/react-native-reanimated/lib/reanimated2/jestUtils.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-native-reanimated/lib/reanimated2/jestUtils' {
+  declare const advanceAnimationByTime: (time: number) => void;
+  declare const withReanimatedTimer: (callback: VoidFunction) => void;
+}
