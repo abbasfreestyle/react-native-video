799cbf6dbb4217587ad114c1ffd03e09d7ece48d
Abbas Farid <abbasfreestyle@gmail.com>
2021-09-19 00:30:13 +0100

    feat: use the play button in a modular way

--- src/screens/ScrolledView.tsx
+++ src/screens/ScrolledView.tsx
@@ -1,18 +1,50 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text } from 'react-native';
 
-import { Video } from '../@abbasfarid/react-native-video';
+import { Play, useVideo, Video } from '../@abbasfarid/react-native-video';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const uri =
+  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4';
+
+const CustomPlay = () => {
+  const { togglePlay } = useVideo();
+  return (
+    <Pressable onPress={togglePlay}>
+      <Text style={styles.customTextColor}>Custom play button</Text>
+    </Pressable>
+  );
+};
+
+/* 
+Idea
+
+<Video src>
+  <Top>
+    <Logo url />
+    <Title text />
+    <More onPress />
+  </Top>
+  <Main>
+    <Rewind />
+    <Play />
+    <FastForward />
+  </Main>
+  <Bottom>
+    <Play />
+    <Scrubber />
+    <Volume />
+    <FullScreen />
+  </Bottom>
+</Video>
+*/
 
 export const ScrolledView = () => {
   return (
     <ScrollView style={styles.container}>
-      <Video />
+      <Video source={{ uri }}>
+        <Play />
+      </Video>
+      <CustomPlay />
       <Text>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rerum
         velit libero, soluta accusantium, ad recusandae nesciunt dicta et iure
@@ -21,3 +53,12 @@ export const ScrolledView = () => {
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  customTextColor: {
+    color: 'white',
+  },
+});
