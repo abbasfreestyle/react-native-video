471c4acf6c91352abf38e8ba982442da5e6b0928
Abbas Farid <abbasfreestyle@gmail.com>
2021-10-09 04:20:25 +0100

    refactor: video component

--- src/@abbasfarid/react-native-video/components/Video/Video.tsx
+++ src/@abbasfarid/react-native-video/components/Video/Video.tsx
@@ -4,28 +4,12 @@ import { StyleSheet, View } from 'react-native';
 import RNVideo, { VideoProperties } from 'react-native-video';
 
 import { useVideo } from '../../context';
-import { Displayer } from '..';
+import { Displayer } from '../Displayer';
 
 export type Props = {
   children?: React.ReactNode;
   source: VideoProperties['source'];
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'black',
-  },
-  video: {
-    alignSelf: 'stretch',
-  },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-  },
-});
 
 export const VideoPlayer = ({ children, source }: Props) => {
   const [height, setHeight] = useState(0);
@@ -54,3 +38,19 @@ export const VideoPlayer = ({ children, source }: Props) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'black',
+  },
+  video: {
+    alignSelf: 'stretch',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+  },
+});
